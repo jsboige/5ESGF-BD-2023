d0b3f9684994164ad73e0d4c64c1ff398e077501
Abdoulaye TRAORE <VQ5962@engie.com>
2023-07-04 13:29:30 +0200

    feat: ajout de notre solver

---
+++ Sudoku.GrapheColor/GrapheColorSolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Sudoku.Shared;
+
+namespace Sudoku.GrapheColor
+{
+    public class GrapheColorSolver : ISudokuSolver
+    {
+        SudokuGrid ISudokuSolver.Solve(SudokuGrid grid)
+        {
+            try
+            {
+                // Chargement du réseau et affichage avant coloration
+                Graphe graphe = new Graphe(grid);
+                Console.WriteLine();
+                Console.WriteLine("Affichage de la grille a completer");
+                graphe.displayGrid();
+
+                // Coloration algorithme naif optimisé
+                Console.WriteLine("Parcours du graphe en utilisant un algorithme naif optimise");
+                graphe.algoNaifOptimise();
+                Console.WriteLine("Affichage du resultat");
+                graphe.displayGrid();
+                Console.WriteLine();
+                Console.WriteLine("Verification du resultat");
+                return graphe.getGrid();
+            }
+            catch (Exception e)
+            {
+                Console.Write("Attention ", e);
+            }
+            return grid;
+        }
+    }
+}
---
+++ Sudoku.GrapheColor/README.md
@@ -0,0 +1,8 @@
+## Sudoku solver
+
+### project realised by :
+- TRAORE Abdoulaye
+- CHACHA Ali Ahmed Bachir
+- Soh Denis 
+- BENDDIF Basma
+- AMRANI Sohaib
