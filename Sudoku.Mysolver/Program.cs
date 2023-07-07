55ea7ec042c3ebe8bafd47c13a11054ae08d4925
fmujani <fmujani@oaas.fr>
2023-07-08 01:36:52 +0200

    adding solver

---
+++ Sudoku.Mysolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mysolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
