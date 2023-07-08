7f0479fd5481972a7ad915e6a41fcd40c67ef66f
RihabTsi <rmessaoudi666@icloud.com>
2023-07-08 02:23:25 +0200

    readme

--- HelloSpark/Program.cs
+++ HelloSpark/Program.cs
@@ -6,46 +6,46 @@ using static Microsoft.Spark.Sql.Functions;
 
 namespace HelloSpark
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello Spark!");
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Spark!");
 
-			// Create Spark session
-			SparkSession spark =
-				SparkSession
-					.Builder()
-					.AppName("word_count_sample")
-					.GetOrCreate();
+            // Create Spark session
+            SparkSession spark =
+                SparkSession
+                    .Builder()
+                    .AppName("word_count_sample")
+                    .GetOrCreate();
 
-			// Create initial DataFrame
-			//string filePath = args[0];
-			//Console.WriteLine($"Current Dir:{Environment.CurrentDirectory}");
-			//Console.WriteLine($"Current Files: {Directory.GetFiles(Environment.CurrentDirectory).Aggregate((s, s1) => s+s1)}");
+            // Create initial DataFrame
+            //string filePath = args[0];
+            //Console.WriteLine($"Current Dir:{Environment.CurrentDirectory}");
+            //Console.WriteLine($"Current Files: {Directory.GetFiles(Environment.CurrentDirectory).Aggregate((s, s1) => s+s1)}");
 
-			string filePath = "dbfs:/input.txt";
-			if (args.Length>0)
-			{
-				filePath = args[0];
-			}
-			
-			DataFrame dataFrame = spark.Read().Text(filePath);
+            string filePath = "C:\\Users\\odfkt\\source\\repos\\RihabTsi\\5ESGF-BD-2023\\HelloSpark\\input.txt";
+            if (args.Length > 0)
+            {
+                filePath = args[0];
+            }
 
-			//Count words
-			DataFrame words =
-				dataFrame
-					.Select(Microsoft.Spark.Sql.Functions.Split(Col("value"), " ").Alias("words"))
-					.Select(Explode(Col("words")).Alias("word"))
-					.GroupBy("word")
-					.Count()
-					.OrderBy(Col("count").Desc());
+            DataFrame dataFrame = spark.Read().Text(filePath);
 
-			// Display results
-			words.Show();
+            //Count words
+            DataFrame words =
+                dataFrame
+                    .Select(Microsoft.Spark.Sql.Functions.Split(Col("value"), " ").Alias("words"))
+                    .Select(Explode(Col("words")).Alias("word"))
+                    .GroupBy("word")
+                    .Count()
+                    .OrderBy(Col("count").Desc());
 
-			// Stop Spark session
-			spark.Stop();
-		}
-	}
+            // Display results
+            words.Show();
+
+            // Stop Spark session
+            spark.Stop();
+        }
+    }
 }
