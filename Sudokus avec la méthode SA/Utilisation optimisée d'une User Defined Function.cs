62d68d0475f255d7890318f0f6076a8f1f57d93b
Jean-Sylvain Boige <jsboige@gmail.com>
2023-07-14 15:11:59 +0200

    tentative de faire fonctionner le code

---
+++ Sudokus avec la méthode SA/Utilisation optimisée d'une User Defined Function.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Common;
+using Microsoft.Spark.Sql;
+using Microsoft.Spark.Sql.Streaming;
+using static Microsoft.Spark.Sql.Functions;
+
+namespace Sudokus_avec_la_méthode_SA
+{
+    internal class Class2
+    {
+        //SparkSession spark = SparkSession
+        //.Builder()
+        //.AppName("Streaming example with a UDF")
+        //.GetOrCreate();
+        //DataFrame lines = spark
+        //.ReadStream()
+        //.Format("socket")
+        //.Option("host", hostname)
+        //.Option("port", port)
+        //.Load();
+        //static void Main()
+        //{
+        //    Func<Column, Column> udfArray =
+        //    Udf<string, string[]>((str) => new string[] { str, $"{str} {str.Length}" });
+        //}
+}
+
+}
---
+++ Sudokus avec la méthode SA/notebook interative.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Spark.Sql;
+
+namespace Sudokus_avec_la_méthode_SA
+{
+    internal class Class1
+    {
+//        var sparkSession =
+//SparkSession
+//    .Builder()
+//    .AppName("dotnet-interactive-spark")
+//    .GetOrCreate();
+    }
+}
